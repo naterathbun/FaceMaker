b4f1ce4b1e0a0fce9b76cc5a335e6ccad8b2717f
Nathan Rathbun <naterathbun@gmail.com>
2017-01-22 15:47:45 -0500

    Laid out main and face class, basic operations

---
+++ FaceMaker/Classes/Face.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceMaker.Classes
+{
+    class Face
+    {
+        private string[] faceDisplayHair = new string[4];       // Hair String Array
+        private string[] faceDisplayEyes = new string[5];       // Eyes String Array
+        private string[] faceDisplayNose = new string[4];       // Nose String Array
+        private string[] faceDisplayMouth = new string[5];      // Mouth String Array
+        private string[] faceDisplayChin = new string[4];       // Chin String Array
+
+        public Face()                                           // Constructor
+        {
+            SetFaceToBlank();
+        }
+
+        public void DisplayFace()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < faceDisplayHair.Length; i++)
+            {
+                Console.WriteLine(faceDisplayHair[i]);
+            }
+
+            for (int i = 0; i < faceDisplayEyes.Length; i++)
+            {
+                Console.WriteLine(faceDisplayEyes[i]);
+            }
+
+            for (int i = 0; i < faceDisplayNose.Length; i++)
+            {
+                Console.WriteLine(faceDisplayNose[i]);
+            }
+
+            for (int i = 0; i < faceDisplayMouth.Length; i++)
+            {
+                Console.WriteLine(faceDisplayMouth[i]);
+            }
+
+            for (int i = 0; i < faceDisplayChin.Length; i++)
+            {
+                Console.WriteLine(faceDisplayChin[i]);
+            }
+        }                            // Prints the current face
+
+        public void SetFaceToBlank()                            // Blank out the whole face
+        {
+            SetHair("                   ooo OOO OOO ooo                   ",
+                    "               oOO                 OOo               ",
+                    "           oOO                         OOo           ",
+                    "        oOO                               OOo        ");
+
+            SetEyes("      oOO                                   OOo      ",
+                    "    oOO                                       OOo    ",
+                    "   oOO                                         OOo   ",
+                    "  oOO                                           OOo  ",
+                    " oOO                                             OOo ");
+
+            SetNose(" oOO                                             OOo ",
+                    " oOO                                             OOo ",
+                    " oOO                                             OOo ",
+                    " oOO                                             OOo ");
+
+            SetMouth("  oOO                                           OOo  ",
+                     "   oOO                                         OOo   ",
+                     "    oOO                                       OOo    ",
+                     "      oOO                                   OOo      ",
+                     "        oO                                OOo        ");
+
+            SetChin("           oOO                         OOo           ",
+                    "               oOO                 OOo               ",
+                    "                   ooo OOO OOO ooo                   ",
+                    "                                                     ");
+        }
+
+        public void SetHair(string lineZero, string lineOne, string lineTwo, string lineThree)
+        {
+            this.faceDisplayHair[0] = lineZero;
+            this.faceDisplayHair[1] = lineOne;
+            this.faceDisplayHair[2] = lineTwo;
+            this.faceDisplayHair[3] = lineThree;
+        }
+
+        public void SetEyes(string lineZero, string lineOne, string lineTwo, string lineThree, string lineFour)
+        {
+            this.faceDisplayEyes[0] = lineZero;
+            this.faceDisplayEyes[1] = lineOne;
+            this.faceDisplayEyes[2] = lineTwo;
+            this.faceDisplayEyes[3] = lineThree;
+            this.faceDisplayEyes[4] = lineFour;
+        }
+
+        public void SetNose(string lineZero, string lineOne, string lineTwo, string lineThree)
+        {
+            this.faceDisplayNose[0] = lineZero;
+            this.faceDisplayNose[1] = lineOne;
+            this.faceDisplayNose[2] = lineTwo;
+            this.faceDisplayNose[3] = lineThree;
+        }
+
+        public void SetMouth(string lineZero, string lineOne, string lineTwo, string lineThree, string lineFour)
+        {
+            this.faceDisplayMouth[0] = lineZero;
+            this.faceDisplayMouth[1] = lineOne;
+            this.faceDisplayMouth[2] = lineTwo;
+            this.faceDisplayMouth[3] = lineThree;
+            this.faceDisplayMouth[4] = lineFour;
+        }
+
+        public void SetChin(string lineZero, string lineOne, string lineTwo, string lineThree)
+        {
+            this.faceDisplayChin[0] = lineZero;
+            this.faceDisplayChin[1] = lineOne;
+            this.faceDisplayChin[2] = lineTwo;
+            this.faceDisplayChin[3] = lineThree;
+        }
+
+
+
+    }
+}
---
+++ FaceMaker/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaceMaker.Classes;
+
+namespace FaceMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Face gameFace = new Face();         // Instantiate the face object
+            DisplayIntro();                     // Splash Screen
+
+            while(true)
+            {
+                gameFace.DisplayFace();         // Prints latest face
+                DisplayMenu();                  // Main Menu
+            }
+
+
+
+            Console.ReadLine();             // pause and keep window open for testing
+        }
+
+        static void DisplayIntro()
+        {
+            Console.WriteLine("                   ooo OOO OOO ooo                   ");
+            Console.WriteLine("               oOO                 OOo               ");
+            Console.WriteLine("           oOO                         OOo           ");
+            Console.WriteLine("        oOO                               OOo        ");
+            Console.WriteLine("      oOO                                   OOo      ");
+            Console.WriteLine("    oOO                                       OOo    ");
+            Console.WriteLine("   oOO      WELCOME               WRITTEN      OOo   ");
+            Console.WriteLine("  oOO       TO FACE              BY NATHAN      OOo  ");
+            Console.WriteLine(" oOO         MAKER                RATHBUN        OOo ");
+            Console.WriteLine(" oOO                                             OOo ");
+            Console.WriteLine(" oOO                                             OOo ");
+            Console.WriteLine(" oOO                   PLEASE                    OOo ");
+            Console.WriteLine(" oOO                                             OOo ");
+            Console.WriteLine("  oOO                                           OOo  ");
+            Console.WriteLine("   oOO      PRESS                    GAME      OOo   ");
+            Console.WriteLine("    oOO           ENTER TO START THE          OOo    ");
+            Console.WriteLine("      oOO                                   OOo      ");
+            Console.WriteLine("        oO                                OOo        ");
+            Console.WriteLine("           oOO                         OOo           ");
+            Console.WriteLine("               oOO                 OOo               ");
+            Console.WriteLine("                   ooo OOO OOO ooo                   ");
+            Console.WriteLine("                                                     ");
+
+            Console.ReadLine();
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine(" [1] Generate a random face.");
+            Console.WriteLine(" [2] Change your face's Hair.");
+            Console.WriteLine(" [3] Change your face's Eyes.");
+            Console.WriteLine(" [4] Change your face's Nose.");
+            Console.WriteLine(" [5] Change your face's Mouth.");
+            Console.WriteLine(" [6] Change your face's Chin.");
+            Console.WriteLine(" [7] Start over with a blank face.");
+            
+            Console.ReadLine();
+
+
+            // Options (Each should call methods in face)
+
+            // Clear Face -- blanks face out and reloads
+            // Random Face -- pulls a random number for each face possibility and reloads
+
+            // each of these launches a method which changes the screen to a choose your whatever 
+
+            // Change Hair
+            // Change Eyes
+            // Change Nose
+            // Change Mouth
+            // Change Chin
+
+        }
+
+
+    }
+}
